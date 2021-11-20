7f2b66fefb9bb4cc6f5a02dc38bb9e86f004a59f
Sani Kamal <sanikamal223@gmail.com>
2021-11-20 13:54:00 +0530

    added boostrap5 and some style

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'smartnotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [
+            BASE_DIR / 'static/templates'
+        ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
